e4420aa49ca72fe939f7edd9a8bdf7eacebf84ba
zhouhanxiaoxiao <981436939@qq.com>
2020-11-26 16:10:21 +0800

    easyInit

---
+++ src/main/java/com/cibr/easyfly/EasyflyApplication.java
@@ -0,0 +1,13 @@
+package com.cibr.easyfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyflyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cibr/easyfly/EasyflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibr.easyfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
